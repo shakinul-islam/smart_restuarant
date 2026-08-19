9110f7fcfdb1f365739eca92c5951c18e960d50f
Md Shakinul Islam <shakinulislam017@gmail.com>
2026-08-20 04:30:10 +0600

    Fixed loader overlapping issue

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2449336912" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3952076794" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
